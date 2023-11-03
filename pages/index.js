580b483428a7c8f6b31fb4be9f36fb303b6c3df1
nazi <nazipaeez777@gmail.com>
2023-11-03 18:34:35 +0330

    Create about section & responsive

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Slider from "@/components/templates/Index/Slider/Slider"
+import About from "@/components/templates/Index/About/About"
 
 export default function Home() {
   return (
     <>
     <Slider/>
+    <About/>
     </>
   )
 }
