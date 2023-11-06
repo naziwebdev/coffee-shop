318ed5f663ef81b23b9fded416dd31d71682b7ff
nazi <nazipaeez777@gmail.com>
2023-11-06 21:09:41 +0330

    Create testimonial page

---
+++ components/templates/Testimonial/TestimonialDetail.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from './TestimonialDetail.module.css'
+import ClientSayItem from '@/components/modules/ClientSayItem/ClientSayItem'
+
+export default function Testimonial({comments}) {
+  return (
+    <div className={styles['testimonial']}>
+    <h3 className='u__section-title'>
+      Testimonial
+    </h3>
+    <h1 className='u__section-text'>
+      our clients say
+    </h1>
+     <div className={styles['testimonial__content']}>
+         {comments?.map(comment => (
+          <ClientSayItem key={crypto.randomUUID()} {...comment} />
+         ))}
+     </div>
+    </div>
+  )
+}
--- pages/testimonial/index.js
+++ pages/testimonial/index.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import Header from '@/components/modules/Header/Header'
+import TestimonialDetail from '@/components/templates/Testimonial/TestimonialDetail'
 
-export default function Testimonial() {
+export default function Testimonial({data}) {
   return (
-    <div>Testimonial</div>
+    <div>
+      <Header route={'testimonial'} />
+      <TestimonialDetail comments={data}/>
+    </div>
   )
+}
+
+export async function getStaticProps() {
+  
+  const res = await fetch('http://localhost:4000/comments')
+  const data = await res.json()
+
+  return {
+    props:{
+      data
+    },
+    revalidate : 60 * 60 * 12
+  }
 }
