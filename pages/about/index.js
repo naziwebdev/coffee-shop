2651316c84a8f5ed14a2f6f499f5d7e1a5d62865
nazi <nazipaeez777@gmail.com>
2023-11-06 20:43:00 +0330

    Create about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '@/components/modules/Header/Header'
+import About from '@/components/templates/Index/About/About'
 
-export default function About() {
+export default function AboutPage() {
   return (
     <div>
-      <Header route={'about'}/>
+      <Header route={'about'} />
+      <About />
     </div>
   )
 }
