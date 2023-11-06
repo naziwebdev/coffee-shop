e13104a249b5dadc631f72b1cf2f91de1e3d4f19
nazi <nazipaeez777@gmail.com>
2023-11-06 21:04:00 +0330

    Create reservation page

--- pages/reservation/index.js
+++ pages/reservation/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import Header from '@/components/modules/Header/Header'
+import ReserveDetail from '@/components/templates/Reservation/ReserveDetail'
 
 export default function Reservation() {
   return (
-    <div>Reservation</div>
+    <div>
+      <Header route={'reservation'} />
+      <ReserveDetail/>
+    </div>
   )
 }
